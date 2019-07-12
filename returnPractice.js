dec18d5651b2ba5afcffd9b37621fca60d5712c4
zacharyeASC6 <zacharyetiennetbls@gmail.com>
2019-07-12 09:46:52 -0400

    Makes function w/ return value

---
+++ returnPractice.js
@@ -0,0 +1,11 @@
+//Every function returns a value. 
+//If one isn't specified, it returns undefined
+//return for functions is like else if after a true if statement.
+//Nothing under it works
+function someFunc(){
+return 8;
+}
+
+const result = someFunc();
+
+console.log(result);
