ee9de3c0f8b502cd3e61a6e9e4de56da226a0f0e
zacharyeASC6 <zacharyetiennetbls@gmail.com>
2019-07-12 09:59:11 -0400

    Makes a function that adds two numbers

--- returnPractice.js
+++ returnPractice.js
@@ -2,6 +2,8 @@
 //If one isn't specified, it returns undefined
 //return for functions is like else if after a true if statement.
 //Nothing under it works
+//Rather than make the function print the thing,
+//have it return that thing and print it outside
 function someFunc(){
 return 8;
 }
@@ -9,3 +11,10 @@ return 8;
 const result = someFunc();
 
 console.log(result);
+
+function addTwoNums(num1, num2){
+   return num1 + num2;
+}
+
+const add = addTwoNums(20,2020);
+console.log(add);
